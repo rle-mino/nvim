fc07a34ee1ddba7e086019cb0471476b10df0107
rle-mino <raphael.leminor@gmail.com>
2025-12-26 18:29:10 +0100

    feat(tree): setup neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,195 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  cmd = "Neotree",
+  keys = {
+    {
+      "<leader>e",
+      function()
+        local neo_tree_winnr = nil
+
+        -- Find neo-tree window
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          local ft = vim.api.nvim_buf_get_option(buf, 'filetype')
+          if ft == 'neo-tree' then
+            neo_tree_winnr = win
+            break
+          end
+        end
+
+        -- If neo-tree is focused, focus the previous window (main buffer)
+        if neo_tree_winnr and vim.api.nvim_get_current_win() == neo_tree_winnr then
+          vim.cmd('wincmd p')
+        -- If neo-tree exists but not focused, focus it
+        elseif neo_tree_winnr then
+          vim.api.nvim_set_current_win(neo_tree_winnr)
+        -- If neo-tree doesn't exist, open it
+        else
+          vim.cmd('Neotree show')
+        end
+      end,
+      desc = "Toggle focus to file explorer"
+    },
+    { "<leader>E", "<cmd>Neotree reveal<cr>", desc = "Reveal current file" },
+  },
+  init = function()
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function(data)
+        -- Check if opening a directory
+        local directory = vim.fn.isdirectory(data.file) == 1
+
+        -- Check if no files were specified
+        local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+        -- Open neo-tree
+        vim.cmd("Neotree show")
+
+        -- If not opening a directory and a file was specified, focus the file
+        if not directory and not no_name then
+          vim.cmd("wincmd p")
+        end
+      end,
+    })
+  end,
+  config = function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    require("neo-tree").setup({
+      close_if_last_window = false,
+      popup_border_style = "rounded",
+      enable_git_status = true,
+      enable_diagnostics = true,
+      open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
+      sort_case_insensitive = true,
+      source_selector = {
+        winbar = true,
+        statusline = false,
+        content_layout = "center",
+        sources = {
+          { source = "filesystem", display_name = " 󰉓 Files " },
+        },
+      },
+      default_component_configs = {
+        container = { enable_character_fade = true },
+        indent = {
+          indent_size = 2,
+          padding = 1,
+          with_markers = true,
+          indent_marker = "│",
+          last_indent_marker = "└",
+          highlight = "NeoTreeIndentMarker",
+          with_expanders = true,
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
+        },
+        icon = {
+          folder_closed = "",
+          folder_open = "",
+          folder_empty = "",
+          default = "*",
+          highlight = "NeoTreeFileIcon",
+        },
+        modified = { symbol = "[+]", highlight = "NeoTreeModified" },
+        name = {
+          trailing_slash = false,
+          use_git_status_colors = true,
+          highlight = "NeoTreeFileName",
+        },
+        git_status = {
+          symbols = {
+            added = "✚",
+            modified = "",
+            deleted = "✖",
+            renamed = "󰁕",
+            untracked = "",
+            ignored = "",
+            unstaged = "~",
+            staged = "",
+            conflict = "",
+          },
+        },
+      },
+      window = {
+        position = "left",
+        width = 35,
+        mapping_options = { noremap = true, nowait = true },
+        mappings = {
+          ["<space>"] = { "toggle_node", nowait = false },
+          ["<2-LeftMouse>"] = "open",
+          ["<cr>"] = "open",
+          ["<esc>"] = "cancel",
+          ["P"] = { "toggle_preview", config = { use_float = true } },
+          ["l"] = "open",
+          ["h"] = "close_node",
+          ["S"] = "open_split",
+          ["s"] = "open_vsplit",
+          ["t"] = "open_tabnew",
+          ["w"] = "open_with_window_picker",
+          ["C"] = "close_node",
+          ["z"] = "close_all_nodes",
+          ["Z"] = "expand_all_nodes",
+          ["a"] = { "add", config = { show_path = "relative" } },
+          ["A"] = "add_directory",
+          ["d"] = "delete",
+          ["r"] = "rename",
+          ["y"] = "copy_to_clipboard",
+          ["x"] = "cut_to_clipboard",
+          ["p"] = "paste_from_clipboard",
+          ["c"] = "copy",
+          ["m"] = "move",
+          ["q"] = "close_window",
+          ["R"] = "refresh",
+          ["?"] = "show_help",
+          ["<"] = "prev_source",
+          [">"] = "next_source",
+          ["i"] = "show_file_details",
+        },
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+          hide_by_name = { ".DS_Store", "thumbs.db" },
+          never_show = {},
+        },
+        follow_current_file = { enabled = true, leave_dirs_open = true },
+        group_empty_dirs = false,
+        hijack_netrw_behavior = "open_default",
+        use_libuv_file_watcher = true,
+        window = {
+          mappings = {
+            ["<bs>"] = "navigate_up",
+            ["."] = "set_root",
+            ["H"] = "toggle_hidden",
+            -- disables file search (since we use telescope)
+            ["/"] = "noop",
+            ["f"] = "noop",
+            ["D"] = "fuzzy_finder_directory",
+            ["#"] = "fuzzy_sorter",
+            ["<c-x>"] = "clear_filter",
+            ["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
+            ["oc"] = { "order_by_created", nowait = false },
+            ["od"] = { "order_by_diagnostics", nowait = false },
+            ["om"] = { "order_by_modified", nowait = false },
+            ["on"] = { "order_by_name", nowait = false },
+            ["os"] = { "order_by_size", nowait = false },
+            ["ot"] = { "order_by_type", nowait = false },
+          },
+        },
+      },
+
+
+    })
+  end,
+}
+
