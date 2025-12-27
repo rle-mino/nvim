f819401f8f53f01225a007e5fc675bdae6a01f5c
rle-mino <raphael.leminor@gmail.com>
2025-12-27 11:25:31 +0100

    feat(notify): replace nvim-notify with snack notify

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -10,6 +10,23 @@ return {
       picker = {},
       -- Terminal integration (used by opencode.nvim)
       terminal = {},
+      -- Notification system
+      notifier = {
+        enabled = true,
+        timeout = 3000,
+        style = "fancy",
+      },
     },
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "VeryLazy",
+        callback = function()
+          -- Setup vim.notify to use snacks
+          vim.notify = function(msg, level, opts)
+            return require("snacks").notifier.notify(msg, level, opts)
+          end
+        end,
+      })
+    end,
   },
 }
